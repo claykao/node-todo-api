24d29ec2468a7abea3d7e007a7562affe6b252c0
Clayton <claytonkao@yahoo.com>
2017-10-22 20:35:47 -0700

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -131,7 +131,7 @@ app.post('/users', (req, res) => {
     //     email: body.email, 
     //     password: body.password
     // });   
-    // can be simplified to var
+    // can be simplified to
     var user = new User(body);
     
     user.save().then(() => {
@@ -148,7 +148,7 @@ app.post('/users', (req, res) => {
     });
 });
 
-// express middleware
+// express middleware (moved to middleware folder)
 // var authenticate = (req, res, next) => {
 //     var token = req.header('x-auth');
 
@@ -168,6 +168,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+// -----------------------------------------------
+// login a user
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
 });
 
 // -----------------------------------------------
